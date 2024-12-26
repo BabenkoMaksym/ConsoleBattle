9a807090793142ce0d065657e58adaeb47bb63b8
MaksymBabenko <Maksym.Babenko.bbn@gmail.com>
2024-12-26 21:09:21 +0100

    Add ability and abilities for hero

--- src/main/java/bbn/Main.java
+++ src/main/java/bbn/Main.java
@@ -1,5 +1,6 @@
 package bbn;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +12,12 @@ public class Main {
         System.out.println("Please enter your name: ");
         final String name = scanner.nextLine();
         final Hero hero = new Hero(name);
-        System.out.println("Hello " + hero.getName() + ", welcome to the ConsoleBattle game!\n Let's start the game");
+        System.out.println("Hello " + hero.getName() + ", welcome to the ConsoleBattle game!\n" +
+                "Let's start the game");
 
+        System.out.println("Your abilities:");
+        for (Map.Entry<Ability, Integer> entry: hero.getAbilities().entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
